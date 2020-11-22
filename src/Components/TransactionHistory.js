e59e5adfed0a7023f64572b39b61a139d2f8b494
Muhammad Abdullah <56094500+MuhammadAb98@users.noreply.github.com>
2020-11-23 01:07:01 +0500

    submit

--- src/Components/TransactionHistory.js
+++ src/Components/TransactionHistory.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React,{useContext} from 'react';
+//import {GlobalContext} from '../context/GlobalState';
 
 export const TransactionHistory = () => {
 
+//const {transactions} = useContext(GlobalContext);
+//console.log(transactions);
 return(
     <div>
     <h3>Transaction History</h3>
